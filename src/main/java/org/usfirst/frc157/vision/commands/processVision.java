0dca3854059d99474366a99b6bfa8af3ee537dd2
Dralthon <37816567+Dralthon@users.noreply.github.com>
2019-01-16 20:26:55 -0500

    not completely sure...

--- src/main/java/org/usfirst/frc157/vision/commands/processVision.java
+++ src/main/java/org/usfirst/frc157/vision/commands/processVision.java
@@ -55,8 +55,8 @@ public class processVision extends Command {
         {
             if (cargo.get(0) != null)
             {
-                double forward = Robot.drive.drivePID.pidCalculate(176, (cargo.width+cargo.height)/2);
-                double turn = Robot.drive.turnPID.pidCalculate(158, cargo.x);
+                double forward = Robot.drive.drivePID.pidCalculate(176, (cargo.get(0).width+cargo.get(0).height)/2);
+                double turn = Robot.drive.turnPID.pidCalculate(158, cargo.get(0).x);
                 Robot.drive.tankDrive(forward, turn);
             }
         }
